4bbd61eed143d5c54df0b10665a97fb0660dac1c
Kenny Hung (he) <khung@instructors.2u.com>
2022-12-12 20:21:42 +0000

    First GET request

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3001
 
+app.get('/api/movies', (req, res) => {
+  res.send('Hello mum')
+})
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
