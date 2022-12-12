67921ca39074cadec4dcb7e226a515eaedba040e
Kenny Hung (he) <khung@instructors.2u.com>
2022-12-12 20:42:52 +0000

    add movie endpoint

--- server.js
+++ server.js
@@ -12,10 +12,19 @@ async function startApplication() {
     database: 'movie_db'
   })
 
-  app.get('/api/movies', async (req, res) => {
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
+
+  app.get('/api/movies', async (__, res) => {
     const [results, _] = await db.query('select name from movies')
     const movieNames = results.map(it => it.name)
     res.json(movieNames)
+  })
+
+  app.post('/api/add-movie', async (req, res) => {
+    const { name } = req.body
+    await db.query(`insert into movies (name) values (?);`, name)
+    res.status(201).end()
   })
 
   app.listen(port, () => {
